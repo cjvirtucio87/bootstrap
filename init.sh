2f461f4c3aa4c1b8e612177b47b7ef5f5d81ef59
CJ Virtucio <cjv287@gmail.com>
2019-04-21 17:44:31 -0400

    added log_debug function; creating staging_dir file to remember staging directory

--- init.sh
+++ init.sh
@@ -5,19 +5,25 @@ LOG_LEVEL="${LOG_LEVEL:-INFO}";
 DOTFILES_URL="${DOTFILES_URL:-git@github.com:cjvirtucio87/dotfiles.git}";
 
 
+log_debug() {
+  if [ $(echo $LOG_LEVEL | awk '{print tolower($0)}') == "debug" ]; then
+    echo "${1}";
+  fi
+}
+
 install() {
   if [ ! -d "${CONFIG_PATH}" ]; then
-    if [ $(echo $LOG_LEVEL | awk '{print tolower($0)}') == "debug" ]; then
-      echo "initializing config folder at ${CONFIG_PATH}";
-    fi
+    log_debug "initializing config folder at ${CONFIG_PATH}";
 
     mkdir "${CONFIG_PATH}";
   fi
 
-  local staging_dir=$(mktemp -d --tmpdir=$CONFIG_PATH);
-  if [ $(echo $LOG_LEVEL | awk '{print tolower($0)}') == "debug" ]; then
-    echo "creating staging directory ${staging_dir}";
-  fi
+  local staging_dir=$(mktemp -d);
+  log_debug "created staging directory ${staging_dir}";
+
+  echo "${staging_dir}" > "${CONFIG_PATH}/staging_dir";
+
+  log_debug "creating staging_dir file with staging_dir, ${staging_dir}";
 }
 
 main() {
