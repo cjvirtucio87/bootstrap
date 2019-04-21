b7ad89be38435946ff2a99d15cd6bc4a423cbc1a
CJ Virtucio <cjv287@gmail.com>
2019-04-21 17:27:39 -0400

    added install subcommand

--- init.sh
+++ init.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-DOTFILES_URL="${DOTFILES_URL:-git@github.com:cjvirtucio87/dotfiles.git}"
+CONFIG_PATH="${HOME}/.bootstrap";
+LOG_LEVEL="${LOG_LEVEL:-INFO}";
+DOTFILES_URL="${DOTFILES_URL:-git@github.com:cjvirtucio87/dotfiles.git}";
+
+
+install() {
+  if [ ! -d "${CONFIG_PATH}" ]; then
+    if [ $(echo $LOG_LEVEL | awk '{print tolower($0)}') == "debug" ]; then
+      echo "initializing config folder at ${CONFIG_PATH}";
+    fi
+
+    mkdir "${CONFIG_PATH}";
+  fi
+}
 
 main() {
+  local subcommand=$1;
+
+  case "${subcommand}" in
+    'install')
+      install;
+      ;;
+    *)
+      echo "unsupported subcommand ${subcommand}";
+      exit 1;
+  esac
+
+  echo "done"; 
 }
+
+main "$@";
